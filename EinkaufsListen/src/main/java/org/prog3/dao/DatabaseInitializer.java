d44222fff52d3bcce57b76ba889ccd9350a8a243
Noelvanelle <ntchindakuegouo@gmail.com>
2025-01-25 13:58:58 +0100

    DatabaseInitializer and ShoppingList Class

--- EinkaufsListen/src/main/java/org/prog3/dao/DatabaseInitializer.java
+++ EinkaufsListen/src/main/java/org/prog3/dao/DatabaseInitializer.java
@@ -1,4 +1,54 @@
 package org.prog3.dao;
 
-public class DatabaseInitializer {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.JDBCType;
+
+import org.jooq.DSLContext;
+import org.jooq.impl.DSL;
+import org.jooq.impl.SQLDataType;
+
+
+    /**
+     * A utility class that is responsible for defining and setting up the database schema when the application starts.
+     * The class ensures that the required database structure exists so the application can store and retrieve data.
+     * Initializes the database schema
+     */
+    public class DatabaseInitializer {
+
+        public static void initialise () {
+            String url = "";
+
+            try (Connection conn = DriverManager.getConnection(url)) {
+                DSLContext create = DSL.using(conn);
+
+                // table Item
+                create.createTableIfNotExists("Item")
+                        .column("id ", SQLDataType.INTEGER.identity(true))
+                        .column("name", SQLDataType.VARCHAR.length(255).nullable(false))
+                        .column("category", SQLDataType.VARCHAR.length(255).nullable(false))
+                        .column("quantity", SQLDataType.INTEGER.nullable(false))
+                        .column("price", SQLDataType.DOUBLE.precision(10, 2).nullable(false))
+                        .constraint(DSL.constraint("PK_Item").primaryKey("id"))
+                        .execute();
+
+                // table User
+                create.createTableIfNotExists("User")
+                        .column("id", SQLDataType.INTEGER.identity(true))
+                        .column("username", SQLDataType.VARCHAR.length(255).nullable(false))
+                        .column("email", SQLDataType.VARCHAR.length(255).nullable(false))
+                        .constraint(DSL.constraint("PK_User").primaryKey("id"))
+                        .execute();
+
+                // table Shopping
+                create.createTableIfNotExists("Shopping")
+                        .column("id", SQLDataType.INTEGER.identity(true))
+                        .column("total_price", SQLDataType.DOUBLE.precision(10, 2).nullable(false))
+                        .execute();
+
+                System.out.println("Connection to the database ");
+            } catch (Exception e) {
+                System.err.println("ERROR while connecting to the database :" + e.getMessage());
+            }
+        }
 }
--- EinkaufsListen/src/main/java/org/prog3/models/ShoppingList.java
+++ EinkaufsListen/src/main/java/org/prog3/models/ShoppingList.java
@@ -1,7 +1,69 @@
 package org.prog3.models;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Represents a shopping list in the application
  */
 public class ShoppingList {
+    private double totalPrice ;
+    public List<Item> items ;
+
+    /**
+     *
+     * @param totalPrice the Price of all Items
+     */
+
+    // Constructor to initialize the  ShoppingList
+    public ShoppingList(double totalPrice) {
+        this.totalPrice = totalPrice;
+        items = new LinkedList<>();
+    }
+
+    // add an item to the Shopping List
+    public void addItem (Item item){
+        items.add(item);
+        updateTotalPrice();
+    }
+
+    // remove an item to the Shopping List
+    public void deleteItem (Item item) {
+        items.remove(item);
+        updateTotalPrice();
+    }
+
+    // update the quantity or price of an existing item in the list
+    public void updateItem (Item item){
+    // Find the item in the list
+        for ( int i = 0 ; i < items.size();i++){
+            Item currentItem = items.get(i);
+            if (currentItem.getName().equals(item.getName())){
+                // update the item Details
+                currentItem.setQuantity(item.getQuantity());
+                currentItem.setPrice(item.getPrice());
+                updateTotalPrice();
+                return;// Exit after updating
+            }
+
+        }
+        // If the items doesn't exist , add it to the list
+        addItem(item);
+
+    }
+    private  void updateTotalPrice (){
+        totalPrice=0;
+        for (Item item : items ){
+            totalPrice += item.getPrice() * item.getQuantity();
+        }
+    }
+    // display the shoppingList and total Price
+   public void display (){
+       System.out.println("Shopping List : ");
+       for(Item item : items ){
+           System.out.println(item.getName() + "Quantity : " +item.getQuantity() + "; Price " + item.getPrice());
+       }
+       System.out.println( "Total Price = " + totalPrice);
+   }
+
 }
