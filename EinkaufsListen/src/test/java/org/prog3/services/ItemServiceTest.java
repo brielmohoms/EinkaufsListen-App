444c24067dcca7bf8d0f49f864339b0ed55ec51d
brielmohoms <brielmohoms@yahoo.com>
2025-02-09 19:20:32 +0100

    Added total_price to DatabaseInitializer

---
+++ EinkaufsListen/src/test/java/org/prog3/services/ItemServiceTest.java
@@ -0,0 +1,32 @@
+package org.prog3.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemServiceTest {
+
+    @Test
+    void addItem() {
+    }
+
+    @Test
+    void deleteItemByName() {
+    }
+
+    @Test
+    void findItemByName() {
+    }
+
+    @Test
+    void updateItemQuantity() {
+    }
+
+    @Test
+    void getAllItems() {
+    }
+
+    @Test
+    void deleteAllItemsOfAShoppingList() {
+    }
+}
