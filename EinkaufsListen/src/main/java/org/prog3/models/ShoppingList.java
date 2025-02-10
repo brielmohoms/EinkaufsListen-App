8edbf39eeb11bd39e57b794977a1869730f97ca2
Noelvanelle <ntchindakuegouo@gmail.com>
2025-02-10 23:13:50 +0100

    new implementation of the ShoppingList class

--- EinkaufsListen/src/main/java/org/prog3/models/ShoppingList.java
+++ EinkaufsListen/src/main/java/org/prog3/models/ShoppingList.java
@@ -1,5 +1,8 @@
 package org.prog3.models;
 
+import org.prog3.dao.ShoppingListDAO;
+
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -7,63 +10,62 @@ import java.util.List;
  * Represents a shopping list in the application
  */
 public class ShoppingList {
+
     private double totalPrice ;
     public List<Item> items ;
+    public String name ;
+    public int id;
 
-    /**
-     *
-     * @param totalPrice the Price of all Items
-     */
+
 
     // Constructor to initialize the  ShoppingList
-    public ShoppingList(double totalPrice) {
-        this.totalPrice = totalPrice;
-        items = new LinkedList<>();
+    public ShoppingList( int id , String name) {
+        this.id =id ;
+        this.name = name ;
+        this.items= new ArrayList<>();
+        this.totalPrice=0.0;
     }
 
     // add an item to the Shopping List
-    public void addItem (Item item){
-        items.add(item);
-        updateTotalPrice();
-    }
-
-    // remove an item to the Shopping List
-    public void deleteItem (Item item) {
-        items.remove(item);
-        updateTotalPrice();
-    }
-
-    // update the quantity or price of an existing item in the list
-    public void updateItem (Item item){
-    // Find the item in the list
-        for ( int i = 0 ; i < items.size();i++){
-            Item currentItem = items.get(i);
-            if (currentItem.getName().equals(item.getName())){
-                // update the item Details
-                currentItem.setQuantity(item.getQuantity());
-                currentItem.setPrice(item.getPrice());
-                updateTotalPrice();
-                return;// Exit after updating
-            }
-
+    public void addShoppingList (Item item){
+        if (item!=null){
+            items.add(item);
+            updateTotalPrice();
         }
-        // If the items doesn't exist , add it to the list
-        addItem(item);
 
     }
+    public void deleteShoppingList (Item item){
+        if (items.contains(item)) {
+            items.remove(item);
+            updateTotalPrice();
+        }
+    }
+
+    public void updateShoppingList (Item oldItem , Item newItem){
+        int index = items.indexOf(oldItem);
+        if (index != -1) {
+            items.set(index, newItem);
+            updateTotalPrice();
+        }
+    }
+
     private  void updateTotalPrice (){
-        totalPrice=0;
+        totalPrice=0.0;
         for (Item item : items ){
             totalPrice += item.getPrice() * item.getQuantity();
         }
     }
     // display the shoppingList and total Price
-   public void display (){
-       System.out.println("Shopping List : ");
-       for(Item item : items ){
-           System.out.println(item.getName() + "Quantity : " +item.getQuantity() + "; Price " + item.getPrice());
-       }
-       System.out.println( "Total Price = " + totalPrice);
-   }
+    public void display (){
+        System.out.println("Shopping List : " + name );
+        for(Item item : items ){
+            System.out.println(item.getName() + " Quantity : " +item.getQuantity() + "; Price " + item.getPrice());
+        }
+        System.out.println( "Total Price = " + totalPrice);
+    }
+
+    public List<Item> getItems(ShoppingListDAO dao) throws Exception {
+        return dao.getItems(this.id);
+    }
 
 }
